86e1300dce98beca1487d8280fca4a96ea5551a8
Harsh-986 <hy025u@gmail.com>
2025-10-02 09:02:25 +0530

    footer

--- app/layout.js
+++ app/layout.js
@@ -44,7 +44,7 @@ export default function RootLayout({ children }) {
             {/* footer */}
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made by Ansu and team...</p>
+                <p>My Final Year Project.</p>
               </div>
             </footer>
 
